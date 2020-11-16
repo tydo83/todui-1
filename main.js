4b1476f2b28c24fb94da6aa32b44f2c053947485
Colin Jaffe <balloonasaurus@gmail.com>
2020-11-16 14:51:56 -0500

    Initial commit of pared-down version.

---
+++ main.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+
+const interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const menu = `
+Your options are:
+
+1. Add a todo.
+2. Remove a todo.
+3. Remove all completed todos.
+4. Toggle a todo's completion status.
+5. Toggle a todo's priority.
+6. Quit.
+
+`
